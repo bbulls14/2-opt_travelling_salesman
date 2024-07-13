39acc9dcafd72865d3be33aaf9404d0b57e59d50
bbulls1 <bbulls@wgu.edu>
2024-07-13 19:40:17 -0400

    working on interface

--- Package.py
+++ Package.py
@@ -6,7 +6,6 @@ from Clock import Clock
 class Package(object):
     def __init__(self, packageID, address, city, state, zipCode, 
                  deadline, weight, specialNote, status = "", ):
-       
 
         self.packageID = packageID
         self.address = address
@@ -21,84 +20,83 @@ class Package(object):
     def __str__(self):
         return (f"PackageID: {self.packageID}, Address: {self.address}, City: {self.city}, ZipCode: {self.zipCode} Deadline: {self.deadline}, Weight: {self.weight}, Status: {self.status}")
     
-    def getPackageDataList():
-        clock = Clock()
-        packageData = []
-        with open('WGUPS Packages.csv', mode = 'r', encoding='UTF-8-sig') as file:
-            csvFile = csv.reader(file, delimiter=',')
-            next(csvFile)
-            for row in (csvFile):
-                packageID = row[0]
-                address = row[1]
-                city = row[2]
-                state = row[3]
-                zipCode = row[4]
-                deadline = row[5]
-                if 'EOD' in deadline:
-                    deadline = datetime.strptime('4:00 PM', '%I:%M %p')
-                else:
-                    deadline = datetime.strptime(deadline, '%I:%M %p')
-                weight = row[6]
-                specialNote = row[7]
-                status = "at the hub"
+def getPackageDataList():
+    packageData = []
+    with open('WGUPS Packages.csv', mode = 'r', encoding='UTF-8-sig') as file:
+        csvFile = csv.reader(file, delimiter=',')
+        next(csvFile)
+        for row in (csvFile):
+            packageID = row[0]
+            address = row[1]
+            city = row[2]
+            state = row[3]
+            zipCode = row[4]
+            deadline = row[5]
+            if 'EOD' in deadline:
+                deadline = datetime.strptime('4:00 PM', '%I:%M %p')
+            else:
+                deadline = datetime.strptime(deadline, '%I:%M %p')
+            weight = row[6]
+            specialNote = row[7]
+            status = "at the hub"
 
-                
-                package = Package(packageID, address, city, state, 
-                                  zipCode, deadline, weight, specialNote, status)
-                packageData.append(package)
-        return packageData
+            
+            package = Package(packageID, address, city, state, 
+                                zipCode, deadline, weight, specialNote, status)
+            packageData.append(package)
+    return packageData
     
-    def organizePackages(self):
-        endOfBusiness = datetime.strptime('4:00 PM', '%I:%M %p')
+def organizePackages():
+    endOfBusiness = datetime.strptime('4:00 PM', '%I:%M %p')
 
-        allPackages = self.getPackageDataList()
+    allPackages = getPackageDataList()
+    
+    truck1 = []
+    truck2 = []
+    truck3 = []
+    
+    index = 0
+    while index < len(allPackages):
+        package = allPackages[index]
+        address = package.address
+        deadline = package.deadline
+        specialNote = package.specialNote
+        pID = int(package.packageID)
         
-        truck1 = []
-        truck2 = []
-        truck3 = []
+        if 'wrong address' in specialNote:
+            truck3.append(allPackages.pop(index))
+            continue
         
-        index = 0
-        while index < len(allPackages):
-            package = allPackages[index]
-            address = package.address
-            deadline = package.deadline
-            specialNote = package.specialNote
-            pID = int(package.packageID)
+        if 'truck 2' in specialNote or address in [p.address for p in truck2]:
+            truck2.append(allPackages.pop(index))
+            continue
             
-            if 'wrong address' in specialNote:
-                truck3.append(allPackages.pop(index))
-                continue
-            
-            if 'truck 2' in specialNote or address in [p.address for p in truck2]:
-                truck2.append(allPackages.pop(index))
-                continue
-                
-            if 'Delayed on flight' in specialNote:
-                truck2.append(allPackages.pop(index))
-                continue
+        if 'Delayed on flight' in specialNote:
+            truck2.append(allPackages.pop(index))
+            continue
 
-            if 'Must be delivered with' in specialNote or pID in {13, 15, 19} or address in [p.address for p in truck1]:
-                truck1.append(allPackages.pop(index))
-                continue
-            
-            if deadline < endOfBusiness:
-                truck1.append(allPackages.pop(index))
-                continue
-            index += 1 
+        if 'Must be delivered with' in specialNote or pID in {13, 15, 19} or address in [p.address for p in truck1]:
+            truck1.append(allPackages.pop(index))
+            continue
+        
+        if deadline < endOfBusiness:
+            truck1.append(allPackages.pop(index))
+            continue
+        index += 1 
 
-        index = 0
-        while index < len(allPackages):
-            if (len(truck3)<=16):
-                truck3.append(allPackages.pop(index))
-                continue
-            
-            if (len(truck2)<=16):
-                truck2.append(allPackages.pop(index))
-                continue
-            
-            if (len(truck1)<-16):
-                truck1.append(allPackages.pop(index))
-                continue
-            index+=1
-                        
-        return truck1, truck2, truck3
+    index = 0
+    while index < len(allPackages):
+        if (len(truck3)<=16):
+            truck3.append(allPackages.pop(index))
+            continue
+        
+        if (len(truck2)<=16):
+            truck2.append(allPackages.pop(index))
+            continue
+        
+        if (len(truck1)<-16):
+            truck1.append(allPackages.pop(index))
+            continue
+        index+=1
+                    
+    return truck1, truck2, truck3
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+from datetime import datetime
 from HashTable import HashTable
 from AdjMatrix import Matrix
-from Package import Package
+from Package import organizePackages
 from Truck import Truck
 
-pkg = Package()
+
 
 matrix1 = Matrix()
 matrix2 = Matrix()
 matrix3 = Matrix()
 
-truck1Pkgs, truck2Pkgs, truck3Pkgs = pkg.organizePackages()
+truck1Pkgs, truck2Pkgs, truck3Pkgs = organizePackages()
 
 truck1 = Truck(truck1Pkgs)
 truck2 = Truck(truck2Pkgs)
@@ -24,7 +25,41 @@ matrix3 = matrix3.matrixAttributes(truck3.packagesOnTruck)
 tour1, distance1 = matrix1.bestPath(truck1)
 tour2, distance2 = matrix2.bestPath(truck2)
 tour3, distance3 = matrix3.bestPath(truck3)
-print('DONE')
+
+
+print('*******************************************************************************\n')
+
+print('Welcome to the WGUPS Delivery Interface\n')
+
+
+print('1. Check the status of a package')
+print('2. Check miles traveled by a truck')
+input('What would you like to do? Type 1, 2, or 3\n')
+# input = int(input)
+
+# if input != 1 or input !=2:
+    
+
+
+timeOK = True
+while timeOK:
+    try:
+        time = input("What time is it? __:__ AM/PM\n")
+        timeObj = datetime.strptime(time, "%I:%M %p")
+    except:
+        print("\nInvalid Input, use the correct format for hour and minute.\nDon't forget the ':' and include AM or PM at the end\n")
+        timeOK = True
+    else:
+        timeOK = False
+
+
+
+
+
+
+
+
+
 
 
 
